9d3b41b3d9ac3afa59ed7d5e66f41b8f6b860581
Weslin-0101 <wesleeylirac12@gmail.com>
2023-02-22 14:41:09 -0300

    feat: adicionado visitar_cidade 100%

--- trabalho/visitar_cidade.c
+++ trabalho/visitar_cidade.c
@@ -1,121 +1,118 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <limits.h>
-#include <string.h>
+#include <stdlib.h>
 
-#define MAX_V 2000
+#define GRAPH_WEIGHT_T int
+#define GRAPH_WEIGHT_SUM(a, b) (a + b)
+#define GRAPH_WEIGHT_LESS(a, b) (a < b)
+#define GRAPH_WEIGHT_INF 2147483647
 
-int graph[MAX_V][MAX_V]; // matriz de adjacência para armazenar as conexões entre as cidades
-int V;                   // número de cidades
-
-bool visited[MAX_V]; // vetor de visitados
-
-typedef enum
+typedef struct
 {
-    IDA_VOLTA,
-    APENAS_IDA,
-    APENAS_VOLTA,
-    IMPOSSIBRU
-} TipoViagem;
+    unsigned from, to;
+    GRAPH_WEIGHT_T weight;
+} Edge;
 
-// algoritmo DFS para percorrer o grafo a partir do vértice atual
-void DFS(int current)
+typedef struct
 {
-    visited[current] = true;
-    for (int i = 0; i < V; i++)
+    GRAPH_WEIGHT_T **edges;
+    unsigned V, E;
+} Graph;
+
+Graph G_init(unsigned numberOfVertices)
+{
+    Graph g = {.V = numberOfVertices, .E = 0};
+    g.edges = malloc(sizeof(GRAPH_WEIGHT_T *) * g.V);
+    for (unsigned i = 0; i < g.V; i++)
     {
-        if (graph[current][i] < INT_MAX && !visited[i])
-        {
-            DFS(i);
-        }
+        g.edges[i] = malloc(sizeof(GRAPH_WEIGHT_T) * g.V);
+        for (unsigned j = 0; j < g.V; j++)
+            g.edges[i][j] = GRAPH_WEIGHT_INF;
     }
+    return g;
 }
 
-// função para verificar se é possível ir de t a x e vice-versa
-TipoViagem check(int t, int x)
+void G_free(Graph g)
 {
-    // inicializa o vetor de visitados com 0
-    memset(visited, 0, sizeof visited);
+    for (unsigned i = 0; i < g.V; i++)
+        free(g.edges[i]);
+    free(g.edges);
+}
 
-    // verifica se é possível ir de t a x
-    DFS(t);
-    bool t_x = visited[x];
+void G_insert(Graph *g, Edge e)
+{
+    if (e.from > g->V || e.to > g->V)
+        return;
 
-    // inicializa o vetor de visitados com 0 novamente
-    memset(visited, 0, sizeof visited);
+    g->E += (g->edges[e.from][e.to] == GRAPH_WEIGHT_INF);
+    g->edges[e.from][e.to] = e.weight;
+}
 
-    // verifica se é possível ir de x a t
-    DFS(x);
-    bool x_t = visited[t];
+int G_numberOfEdges(Graph g)
+{
+    return g.E;
+}
 
-    if (t_x && x_t)
+int G_numberOfVertices(Graph g)
+{
+    return g.V;
+}
+
+void G_components(Graph g, int start, int components[])
+{
+    if (components[start] < 0)
+        components[start] = start;
+    for (unsigned i = 0; i < g.V; i++)
+        if (g.edges[start][i] != GRAPH_WEIGHT_INF && components[i] < 0)
+        {
+            int mn = ((int)i < components[start] ? (int)i : components[start]);
+            components[start] = mn;
+            components[i] = mn;
+            G_components(g, i, components);
+        }
+}
+
+void G_transitiveClosure(Graph g, int transitive[][2001])
+{
+    for (unsigned i = 0; i < g.V; i++)
     {
-        return IDA_VOLTA;
+        for (unsigned j = 0; j < g.V; j++)
+            transitive[i][j] = g.edges[i][j] != GRAPH_WEIGHT_INF;
+        transitive[i][i] = 1;
     }
-    else if (t_x)
+
+    for (unsigned i = 0; i < g.V; i++)
+        for (unsigned j = 0; j < g.V; j++)
+            if (transitive[j][i] == 1)
+                for (unsigned k = 0; k < g.V; k++)
+                    if (transitive[i][k] == 1)
+                        transitive[j][k] = 1;
+}
+
+int tc[2001][2001];
+
+void solve()
+{
+    int v;
+    scanf(" %d", &v);
+
+    Graph g = G_init(v);
+    for (int f, t, d; scanf(" %d %d %d", &f, &t, &d) == 3 && d;)
     {
-        return APENAS_IDA;
+        G_insert(&g, (Edge){.from = f, .to = t, .weight = 1});
+        if (d == 2)
+            G_insert(&g, (Edge){.from = t, .to = f, .weight = 1});
     }
-    else if (x_t)
-    {
-        return APENAS_VOLTA;
-    }
-    else
-    {
-        return IMPOSSIBRU;
-    }
+
+    G_transitiveClosure(g, tc);
+    G_free(g);
+
+    for (int t, x; scanf(" %d %d", &t, &x) == 2;)
+        printf("%s\n", (tc[t][x] && tc[x][t] ? "Ida e Volta" : (tc[t][x] ? "Apenas Ida" : (tc[x][t] ? "Apenas Volta" : "Impossibru"))));
 }
 
 int main()
 {
-    int v, w, d;
-    scanf("%d", &V);
-
-    // inicializa a matriz de adjacência com INT_MAX
-    for (int i = 0; i < V; i++)
-    {
-        for (int j = 0; j < V; j++)
-        {
-            graph[i][j] = INT_MAX;
-        }
-    }
-
-    while (true)
-    {
-        scanf("%d %d %d", &v, &w, &d);
-        if (v == 0 && w == 0 && d == 0)
-        {
-            break;
-        }
-
-        // adiciona a conexão entre as cidades na matriz de adjacência
-        graph[v][w] = d;
-        if (d == 2)
-        {
-            graph[w][v] = 1;
-        }
-    }
-
-    int t, x;
-    while (scanf("%d %d", &t, &x) != EOF)
-    {
-        TipoViagem tipo = check(t, x);
-        switch (tipo)
-        {
-        case IDA_VOLTA:
-            printf("Ida e Volta\n");
-            break;
-        case APENAS_IDA:
-            printf("Apenas Ida\n");
-            break;
-        case APENAS_VOLTA:
-            printf("Apenas Volta\n");
-            break;
-        case IMPOSSIBRU:
-            printf("Impossibru\n");
-            break;
-        }
-    }
+    solve();
 
     return 0;
 }
